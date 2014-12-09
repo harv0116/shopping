57c92bb0ea44b7852e20b2d65c50cfba7f4d57e5
Paul Harvey <harv0116@algonquinlive.com>
2014-12-09 13:24:53 -0500

    final commit

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,10 @@
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\classes.dex : \
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\classes\ca\edumedia\a\harv0116\shopping\BuildConfig.class \
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\classes\ca\edumedia\a\harv0116\shopping\CordovaApp.class \
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\classes\ca\edumedia\a\harv0116\shopping\R$attr.class \
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\classes\ca\edumedia\a\harv0116\shopping\R$drawable.class \
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\classes\ca\edumedia\a\harv0116\shopping\R$string.class \
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\classes\ca\edumedia\a\harv0116\shopping\R$xml.class \
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\classes\ca\edumedia\a\harv0116\shopping\R.class \
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\classes\org\apache\cordova\device\Device.class \
+C:\wamp\www\mad9014\shopping\platforms\android\ant-build\dexedLibs\classes-ee90cd629e5871d86248f9a22455e1db.jar \
